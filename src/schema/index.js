3158f5c5c8b9d4164d54364ccc673a084943a08a
Paul Cuichan <ing.paul.cuixan@gmail.com>
2017-08-18 08:08:21 -0500

    Archivo shema modificado

--- src/schema/index.js
+++ src/schema/index.js
@@ -8,9 +8,22 @@ type User {
   email: String!
   country: String!
   adress: String!
+  solicitudes: [Solicitude]
+}
+type Solicitude {
+  id: ID!
+  name: String!
+  lastName: String!
+  email: String!
+  country: String!
+  adress: String!
+  delivery: User!
 }
 type Query {
+  solicitudes: [Solicitude]
   users: [User]
+  user(id: Int): User
+  solicitude(id: Int): Solicitude
 }
 `
 const schema = makeExecutableSchema({
